3faf661606ef214afb8ce82c212aae7c5a906baf
Donald Hutchison <donald.hutchison@clabs.co>
2022-11-09 18:18:31 +0100

    Dockerfile for mix release.

--- telegram/mix.exs
+++ telegram/mix.exs
@@ -14,7 +14,7 @@ defmodule TelegramService.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :prometheus_ex],
+      extra_applications: [:logger, :prometheus_ex, :connection],
       mod: {TelegramService.Application, []}
     ]
   end
