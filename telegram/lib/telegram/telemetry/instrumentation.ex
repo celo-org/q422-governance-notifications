277e6f509ad91b0ecbfac697df3550c5f1f2283b
Donald Hutchison <donald.hutchison@clabs.co>
2022-11-10 16:04:53 +0100

    Prefix metrics.

--- telegram/lib/telegram/telemetry/instrumentation.ex
+++ telegram/lib/telegram/telemetry/instrumentation.ex
@@ -2,11 +2,11 @@ defmodule TelegramService.Telemetry.Instrumentation do
   use Prometheus.Metric
 
   @counters [
-    %{metric_name: "received_events", help: "Number of events received on the api endpoint", telemetry_id: :received_event },
-    %{metric_name: "received_telegram_messages", help: "Number of messages received via telegram", telemetry_id: :received_msg },
-    %{metric_name: "sent_telegram_messages", help: "Number of messages sent via telegram", telemetry_id: :sent_msg },
-    %{metric_name: "subscriptions", help: "Number of subscriptions", telemetry_id: :subscribe },
-    %{metric_name: "unsubscribe", help: "Number of unsubscriptions", telemetry_id: :unsubscribe },
+    %{metric_name: "telegram_received_events", help: "Number of events received on the api endpoint", telemetry_id: :received_event },
+    %{metric_name: "telegram_received_telegram_messages", help: "Number of messages received via telegram", telemetry_id: :received_msg },
+    %{metric_name: "telegram_sent_telegram_messages", help: "Number of messages sent via telegram", telemetry_id: :sent_msg },
+    %{metric_name: "telegram_subscriptions", help: "Number of subscriptions", telemetry_id: :subscribe },
+    %{metric_name: "telegram_unsubscribe", help: "Number of unsubscriptions", telemetry_id: :unsubscribe },
   ]
 
   def setup() do
